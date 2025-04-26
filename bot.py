0bd2a8392542a91c7acd69d6c7ad749d95f4daaa
ibeloborodove <goga.beloborodov20@mail.ru>
2025-04-26 12:13:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import requests
 
 TOKEN = "7731694868:AAGZvbAApn7hG7Xl-NnR1YMIfinKCTgtjvo"
 SUPABASE_URL = "https://joiujlddyuspbtmxzwhw.supabase.co"
-SUPABASE_API_KEY = "твой anon ключ"
+SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvaXVqbGRkeXVzcGJ0bXh6d2h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NTI2NDMsImV4cCI6MjA2MTIyODY0M30.USQ8PFMBIhDyJ-Bt6KvdRy89dc2chFiyFBO-rtVlfmw"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
