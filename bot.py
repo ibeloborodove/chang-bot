ce2874ba31bbf9098645b12bf4d345998d4ff923
ibeloborodove <goga.beloborodov20@mail.ru>
2025-04-26 12:09:51 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,34 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+import requests
+
+TOKEN = "7731694868:AAGZvbAApn7hG7Xl-NnR1YMIfinKCTgtjvo"
+SUPABASE_URL = "https://joiujlddyuspbtmxzwhw.supabase.co"
+SUPABASE_API_KEY = "твой anon ключ"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(msg: types.Message):
+    await msg.answer("Привет! Напиши название блюда, и я покажу его ТТК.")
+
+@dp.message_handler()
+async def get_ttk(msg: types.Message):
+    query = msg.text
+    headers = {
+        "apikey": SUPABASE_API_KEY,
+        "Authorization": f"Bearer {SUPABASE_API_KEY}"
+    }
+    res = requests.get(
+        f"{SUPABASE_URL}/rest/v1/ttk?dish_name=eq.{query}",
+        headers=headers
+    )
+    data = res.json()
+    if data:
+        item = data[0]
+        await msg.answer(f"🔹 {item['dish_name']}\n📎 {item['ttk_url']}\n💬 {item['description']}")
+    else:
+        await msg.answer("Такого блюда не нашлось 🤷")
+
+executor.start_polling(dp)
